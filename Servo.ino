893a0907c1944a91b7a6a9f61698a19dfa5902fc
ron <ahsanronykusuma@gmail.com>
2025-10-10 12:57:42 +0700

    Update Servo.ino

--- Servo.ino
+++ Servo.ino
@@ -8,9 +8,30 @@ void capit(){
   delay(200);
 }
 
-void resetposisiongriper(){G1 = 0;}
+unsigned long lastReposTime = 0;   // waktu terakhir reset
+const unsigned long reposDelay = 500; // delay 500 ms antar reset
 
-void resetposisiYongriper(){G1 = 144;}
+void ReposisionGripper() {
+  unsigned long now = millis();
+  if (now - lastReposTime < reposDelay) return; // cegah double trigger
+
+  lastReposTime = now; // update waktu terakhir
+
+  if (G1 > 0 && G1 <= 72 || G1 == 144) {
+    // kalau di bawah tengah atau posisi bawah, pindah ke atas
+    G1 = 0;
+  } 
+  else if (G1 < 144 && G1 > 72 || G1 == 0) {
+    // kalau di atas tengah atau posisi atas, pindah ke bawah
+    G1 = 144;
+  }
+  
+  Serial.print("Reposisi ke: ");
+  Serial.println(G1);
+}
+// void resetposisiongriper(){G1 = 0;}
+
+// void resetposisiYongriper(){G1 = 144;}
 
 void griperhead1() {
   if (Up && G1 <= 140) {G1+=4; Serial.println(G1); delay(servoSpeed);}
